e9a5cabc6928cfdc6fa3b5c740d405adc2c35ad4
Bao Long <long29031997@gmail.com>
2021-03-30 21:49:58 +0700

    Add validation

--- src/orders/dto/create-order.input.ts
+++ src/orders/dto/create-order.input.ts
@@ -1,7 +1,9 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 @InputType()
 export class CreateOrderInput {
   @Field({ description: 'Example field (placeholder)' })
+  @IsEmail({}, { message: 'Vui long nhap email' })
   description: string;
 }
